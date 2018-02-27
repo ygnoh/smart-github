6c356d5a9f743340b73a428481026cb8b14cab4f
yonggoonoh <nohyg90@gmail.com>
2018-02-28 02:33:24 +0900

    feat(#3): Implement options for labels reflected in select menu

--- extension/options.js
+++ extension/options.js
@@ -1,32 +1,30 @@
 // Saves options to chrome.storage
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
+    const label = document.getElementById("label").value;
     chrome.storage.sync.set({
-      favoriteColor: color,
-      likesColor: likesColor
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+        label
+    }, function () {
+        // Update status to let user know options were saved.
+        const status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
     });
-  }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-      favoriteColor: 'red',
-      likesColor: true
-    }, function(items) {
-      document.getElementById('color').value = items.favoriteColor;
-      document.getElementById('like').checked = items.likesColor;
+        favoriteColor: 'red',
+        likesColor: true
+    }, function (items) {
+        document.getElementById('color').value = items.favoriteColor;
+        document.getElementById('like').checked = items.likesColor;
     });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
       save_options);
