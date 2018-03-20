a42e35eaf06f5a131be958bbcbba0f3ce5b047c8
yonggoonoh <nohyg90@gmail.com>
2018-03-21 02:51:18 +0900

    feat(#15): hosts fetch하여 regex 생성

--- src/background.js
+++ src/background.js
@@ -1,9 +1,7 @@
-let rxGithubIssuesOrPulls;
-chrome.storage.sync.get("sg-hosts", result => {
-    const defaultHost = "github.com";
-    const hosts = result["sg-hosts"] || [];
-    hosts.push(defaultHost);
+// default regex that will never match anything
+let rxGithubIssuesOrPulls = /(?!)/;
 
+fetchHosts().then(hosts => {
     const rxHosts = hosts.join("|");
     rxGithubIssuesOrPulls = new RegExp(`^https?:\/\/(www\.)?(?:${rxHosts})\/.*?\/(?:issues|pulls)\/?$`, "i");
 });
@@ -23,3 +21,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         chrome.tabs.sendMessage(tabId, "url-updated");
     }
 });
+
+function fetchHosts() {
+    return new Promise((resolve, reject) => {
+            chrome.storage.sync.get({"sg-hosts": []}, result => {
+            const defaultHost = "github.com";
+            const hosts = result["sg-hosts"] || [defaultHost];
+
+            resolve(hosts);
+        });
+    });
+}
