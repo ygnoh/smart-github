b98e047d932f5170c7a482a36977e3899fb2694a
yonggoonoh <nohyg90@gmail.com>
2018-03-21 02:57:48 +0900

    feat(#15): popup을 통해 hosts 저장

---
+++ src/popup.js
@@ -0,0 +1,42 @@
+fetchHosts().then(hosts => {
+    const hostListContainer = document.querySelector(".sg-list-container");
+
+    hosts.forEach(host => {
+        const item = document.createElement("li");
+        item.innerHTML = host;
+        hostListContainer.appendChild(item);
+    });
+});
+
+const saveBtn = document.getElementById("sg-host-save");
+const input = document.getElementById("sg-host-input");
+saveBtn.addEventListener("click", saveHost);
+
+function saveHost() {
+    const newHost = input.value;
+
+    chrome.storage.sync.get("sg-hosts", result => {
+        const defaultHost = "github.com";
+        const hosts = result["sg-hosts"] || [defaultHost];
+
+        // prevent duplicate hosts
+        if (hosts.includes(newHost)) {
+            return;
+        }
+
+        hosts.push(newHost);
+
+        chrome.storage.sync.set({"sg-hosts": hosts}, () => {
+            location.reload();
+        });
+    });
+}
+
+async function fetchHosts() {
+    return await new Promise((resolve, reject) => {
+        chrome.storage.sync.get("sg-hosts", result => {
+            const hosts = result["sg-hosts"] || ["github.com"];
+            resolve(hosts);
+        });
+    });
+}
