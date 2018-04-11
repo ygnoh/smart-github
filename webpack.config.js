6352582a65462ee42b6805e15e50fe898652579e
yonggoonoh <nohyg90@gmail.com>
2018-04-12 03:17:38 +0900

    refact(#69): env에 따라 webpack config 분기 처리

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/background.js',
-  output: {
-    filename: 'background.bundle.js',
-    path: path.resolve(__dirname, 'dist')
+module.exports = function (env, args) {
+  const config = {
+    entry: './src/background.js',
+    output: {
+      filename: 'background.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      ]
+    }
+  };
+
+  if (env && env.production) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+    config.devtool = 'source-map';
   }
+
+  return config;
 };
