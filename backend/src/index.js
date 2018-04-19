51b16320278fd4a8e60f57bc9c6382765bd536b5
yonggoonoh <nohyg90@gmail.com>
2018-04-20 02:43:43 +0900

    feat(#81): 기초적인 webhook request 처리

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.get("/", (req, res) => res.send("hello backend world!"));
+app.post("/watch", (req, res, next) => {
+    let payload = "";
 
-app.listen(3000, () => console.log("Example app listening on port 3000 for YOU"));
+    req.on("readable", () => {
+        const read = req.read();
+        if (!read) {
+            return;
+        }
+
+        payload += read;
+    });
+
+    req.on("end", () => {
+        const {comment} = JSON.parse(payload);
+        const {html_url, user, body} = comment;
+
+        console.log(`${user.login}가 당신의 글(${html_url})에 댓글을 달았습니다:\n${body}`);
+    });
+});
+
+app.listen(PORT, () => console.log(`\nStart to listen on port ${PORT}.\n`));
