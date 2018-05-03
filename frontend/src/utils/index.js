31d316447f00a864f91a7285402fcb4941ae1b7e
yonggoonoh <nohyg90@gmail.com>
2018-05-04 04:28:10 +0900

    feat(#81): firebase util export

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,10 +2,12 @@ import fetcher from "./fetcher";
 import storage from "./storage";
 import urlManager from "./urlManager";
 import dom from "./dom";
+import firebase from "./firebase";
 
 export {
     fetcher,
     storage,
     urlManager,
-    dom
+    dom,
+    firebase
 };
